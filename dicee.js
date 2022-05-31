3d67b3083ffa07963cecf00d614b56b54fe9d3d0
gailav <97784892+gailav@users.noreply.github.com>
2022-05-31 16:12:02 +0300

    Added initial files

---
+++ dicee.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random()*6) +1;
+var randomPic = "dice" + randomNumber1 +".png"
+document.querySelector(".img1").setAttribute("src", randomPic);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomPic2 = "dice" + randomNumber2 +".png";
+document.querySelector(".img2").setAttribute("src", randomPic2);
+
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw";
+}
